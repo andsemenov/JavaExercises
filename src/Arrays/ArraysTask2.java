ac1489df90dc25e7a9a9acf991ef1c898671e73b
Andrei <andsemenov@gmail.com>
2021-12-26 17:41:23 +0000

    added ArraysTask2

---
+++ src/Arrays/ArraysTask2.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ArraysTask2 {
+    public static void main(String[] args) {
+        String[] weekDays = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println(weekDays[weekDays.length-1]);
+    }
+}
+
+//Задача 2. Создать массив типа String с размером 7.
+// Записать в него значения дней недели.
+// Вывести на консоль значение последнего элемента.
