3338196502d4affa61aa6989fbdd04faf62d461c
Andrei <andsemenov@gmail.com>
2021-12-26 21:32:37 +0000

    added ArraysTask6

--- src/Arrays/ArraysTask6.java
+++ src/Arrays/ArraysTask6.java
@@ -13,7 +13,7 @@ public class ArraysTask6 {
             }
         }
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + "  ");
         }
         System.out.println("\n" + "Maximum is " + max);
     }
