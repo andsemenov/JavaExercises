0f986da80f2c4f24b2a5f3d8c5ce57cbc591ceaa
Andrei <andsemenov@gmail.com>
2021-12-29 21:42:23 +0000

    added Arrays2Task5_refactored

---
+++ src/Arrays2/Arrays2Task5_refactored.java
@@ -0,0 +1,29 @@
+package Arrays2;
+
+public class Arrays2Task5_refactored {
+    public static void main(String[] args) {
+        int[] array = new int[12];
+        int positive = 0;
+        int negative = 0;
+        int length = array.length;
+        int number = length / 2;
+        for (int i = 0; i < length; i++)
+        {
+            int temporary;
+            do
+            {
+                int lowRange = -10;
+                int highRange = 10;
+                temporary = array[i] = (int) (Math.random() * ((highRange - lowRange) + 1)) + lowRange;
+            } while (temporary == 0 ||
+                    (temporary < 0 && negative == (length + 1) / 2) ||
+                    (temporary > 0 && positive == (length + 1) / 2));
+            if ( temporary < 0 ) ++negative;
+            else ++positive;
+            array[i] = temporary;
+        }
+        for (int i = 0; i < length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
