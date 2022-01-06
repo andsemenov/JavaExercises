82335d21b4308e4a6d747600d0f4699df00b36a1
Andrei <andsemenov@gmail.com>
2022-01-06 17:05:18 +0000

    added validation for input

--- src/Methods/MethodsTask5.java
+++ src/Methods/MethodsTask5.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class MethodsTask5 {
     private static int min, max;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter a first integer: ");
-        int number1 = scanner.nextInt();
+        int number1 = inputNumber();
         System.out.println("Enter a second integer: ");
-        int number2 = scanner.nextInt();
+        int number2 = inputNumber();
         System.out.println("The random integer from the range is " + returnNumberFromRange(number1, number2));
-        scanner.close();
     }
+
+    static int inputNumber() {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextFloat()) {
+            System.out.println("It should be a number!");
+            scanner.next();
+        }
+        int number = scanner.nextInt();
+        return number;
+    }
+
     static int returnNumberFromRange(int num1, int num2) {
         findMinMax(num1, num2);
         return (int)(Math.random() * ((max - min) + 1)) + min;
     }
+
     static void findMinMax(int num1, int num2) {
         if (num1 <= num2) {
             min = num1;
