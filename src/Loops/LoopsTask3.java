2bd25552c72d01b533ab38709cf8e8563fc3f4aa
Andrei <andsemenov@gmail.com>
2022-01-09 01:17:13 +0000

    added LoopsTask3

---
+++ src/Loops/LoopsTask3.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class LoopsTask3 {
+    public static void main(String[] args) {
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+//Задача 3. Вывести на консоль последовательность чисел: 7 14 21 28 35 42 49 56 63 70 77 84 91 98.
+// Использовать циклы.
