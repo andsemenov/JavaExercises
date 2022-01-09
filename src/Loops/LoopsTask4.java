2bc88683398adfc2e773da3465d76a282c4a5701
Andrei <andsemenov@gmail.com>
2022-01-09 01:19:29 +0000

    added LoopsTask4

---
+++ src/Loops/LoopsTask4.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class LoopsTask4 {
+    public static void main(String[] args) {
+        for (int row =0; row < 7; row ++) {
+            for (int line = 0; line < 10; line ++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//Задача 4. Вывести на экран снег (зима же все таки)):
+//* * * * * * * * * *
+//* * * * * * * * * *
+//* * * * * * * * * *
+//* * * * * * * * * *
+//* * * * * * * * * *
+//* * * * * * * * * *
+//* * * * * * * * * *
+//Использовать циклы.
