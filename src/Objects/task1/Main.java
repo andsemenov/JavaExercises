9bfb8db8d39ad7b0f96538f2c8be0052cca7889a
Andrei <andsemenov@gmail.com>
2022-01-20 10:03:50 +0000

    added task1

---
+++ src/Objects/task1/Main.java
@@ -0,0 +1,29 @@
+package Objects.task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+
+
+        student1.setFirstName("Joe");
+        student1.setLastName("Burns");
+        student1.setAge(24);
+        student1.setAverageRating(4.2);
+        System.out.println(student1.getFirstName() + " " + student1.getLastName() + ", age: " + student1.getAge() +
+                ", average rating: " + student1.getAverageRating());
+        student1.study();
+        System.out.println();
+        Student student2 = new Student("Alex", "Smith");
+        student2.setAge(28);
+        student2.setAverageRating(4.4);
+        System.out.println(student2.getFirstName() + " " + student2.getLastName() + ", age: " + student2.getAge() +
+                ", average rating: " + student2.getAverageRating());
+        student2.relax();
+        System.out.println();
+        Student student3 = new Student("John", "Brown", 23, 4.7);
+        System.out.println(student3.getFirstName() + " " + student3.getLastName() + ", age: " + student3.getAge() +
+                ", average rating: " + student3.getAverageRating());
+        student3.study();
+        System.out.println();
+    }
+}
---
+++ src/Objects/task1/Student.java
@@ -0,0 +1,79 @@
+package Objects.task1;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double averageRating;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = -1;
+        this.averageRating = -1;
+    }
+
+    public Student(String firstName, String lastName, int age, double averageRating) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.averageRating = averageRating;
+    }
+
+    public void study() {
+        System.out.println("I am studying!");
+    }
+
+    public void relax() {
+        System.out.println("I am relaxing!");
+    }
+
+    public void initialize (String firstName, String lastName, int age, double averageRating) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.averageRating = averageRating;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
+    }
+}
+
+//Задача 1.
+//Создать класс Student, который содержит:
+//a) поля firstName, lastName, age, averageRating.
+//б) методы study() и relax(), в которых просто вывести на консоль текстовые сообщения.
+//в) Добавьте три конструктора  - Student(), Student(firstName, lastName), Student(firstName, lastName, age, averageRating).
+//Создайте три объекта этого класса. Каждый объект должен быть инициализирован разным конструктором.
